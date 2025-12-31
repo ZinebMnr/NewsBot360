7d9e1b13df3dbacb75fc40ad93e54af2dadf22dc
aymanevx <aymane.aibichi@gmail.com>
2025-12-31 15:05:08 +0100

    ajout outil de résumé

--- mcp_server/app.py
+++ mcp_server/app.py
@@ -1,5 +1,5 @@
 from mcp.server.fastmcp import FastMCP
-from mcp_server.tools import pdf_tools, sentiment_tools, news_tools
+from mcp_server.tools import pdf_tools, sentiment_tools, news_tools, summartization_tools
 
 def create_server() -> FastMCP:
     mcp = FastMCP("PDF-and-sentiment")
@@ -7,5 +7,6 @@ def create_server() -> FastMCP:
     pdf_tools.register(mcp)
     sentiment_tools.register(mcp)
     news_tools.register(mcp)
+    summartization_tools.register(mcp)
 
     return mcp
